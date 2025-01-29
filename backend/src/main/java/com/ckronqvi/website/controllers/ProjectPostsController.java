fb2e5dc4c06eb1678080f4868768d8261068474f
Ckronqvi <nooa.kronqvist@gmail.com>
2025-01-29 23:46:36 +0200

    feat: 🎸 add DELETE and UPDATE to project post
    this will add delete and update API endpoints to project post

--- backend/src/main/java/com/ckronqvi/website/controllers/ProjectPostsController.java
+++ backend/src/main/java/com/ckronqvi/website/controllers/ProjectPostsController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +48,16 @@ public class ProjectPostsController {
         return ResponseEntity.status(HttpStatus.CREATED).body(createdProjectPost);
     }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteProjectPost(@CurrentUser User user, @PathVariable("id") Long id) {
+        projectPostsService.deleteProjectPost(user, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateProjectPost(@CurrentUser User user, @PathVariable("id") Long id,
+            @RequestBody ProjectPostRequestDTO projectPostUpdateDTO) {
+        projectPostsService.updateProjectPost(user, id, projectPostUpdateDTO);
+        return ResponseEntity.noContent().build();
+    }
 }
--- backend/src/main/java/com/ckronqvi/website/services/ProjectPostsService.java
+++ backend/src/main/java/com/ckronqvi/website/services/ProjectPostsService.java
@@ -16,6 +16,7 @@ import com.ckronqvi.website.DTOMappers.ProjectPostResponseDTOMapper;
 import com.ckronqvi.website.DTOMappers.ProjectPostSmallDTOMapper;
 import com.ckronqvi.website.enums.ProjectStatus;
 import com.ckronqvi.website.exceptions.ResourceNotFoundException;
+import com.ckronqvi.website.exceptions.UnauthorizedException;
 
 import lombok.AllArgsConstructor;
 
@@ -27,14 +28,14 @@ public class ProjectPostsService {
 
     public List<ProjectPostResponseSmallDTO> getProjectPosts() {
         return projectPostsRepository.findAll().stream()
-            .map(new ProjectPostSmallDTOMapper())
-            .collect(Collectors.toList());
+                .map(new ProjectPostSmallDTOMapper())
+                .collect(Collectors.toList());
     }
 
     public ProjectPostResponseDTO getProjectPostById(Long id) {
         return projectPostsRepository.findById(id)
-            .map(projectPostResponseDTOMapper)
-            .orElseThrow(() -> new ResourceNotFoundException("Project post not found"));
+                .map(projectPostResponseDTOMapper)
+                .orElseThrow(() -> new ResourceNotFoundException("Project post not found"));
     }
 
     public ProjectPostResponseDTO createProjectPost(User user, ProjectPostRequestDTO projectPostCreationDTO) {
@@ -46,5 +47,26 @@ public class ProjectPostsService {
         projectPost.setCreatedAt(LocalDateTime.now());
         projectPost.setUpdatedAt(LocalDateTime.now());
         return projectPostResponseDTOMapper.apply(projectPostsRepository.save(projectPost));
+    }
+
+    public void deleteProjectPost(User user, Long id) {
+        ProjectPost projectPost = projectPostsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Project post not found"));
+        if (!projectPost.getRecruiter().equals(user)) {
+            throw new UnauthorizedException("You are not allowed to delete this project post");
+        }
+        projectPostsRepository.delete(projectPost);
+    }
+
+    public void updateProjectPost(User user, Long id, ProjectPostRequestDTO projectPostUpdateDTO) {
+        ProjectPost projectPost = projectPostsRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Project post not found"));
+        if (!projectPost.getRecruiter().equals(user)) {
+            throw new UnauthorizedException("You are not allowed to update this project post");
+        }
+        projectPost.setTitle(projectPostUpdateDTO.title());
+        projectPost.setDescription(projectPostUpdateDTO.description());
+        projectPost.setUpdatedAt(LocalDateTime.now());
+        projectPostsRepository.save(projectPost);
     }
 }
