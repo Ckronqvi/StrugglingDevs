be20f5a25394769c808e57ec8a5c8ffd86fe1c9e
Ckronqvi <nooa.kronqvist@gmail.com>
2025-02-13 12:55:34 +0200

    fix: 🐛 Fix Docker build scripts
    Java version in backend/Dockerfile did not work with Lombok. Copied
    Dockerfile from nextjs docs to frontend/Dockerfile.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 const nextConfig = {
-    env: {
-      NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
-    },
-  };
-  
-  module.exports = nextConfig;
+  env: {
+    NEXT_PUBLIC_API_URL:
+      process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080",
+  },
+  output: "standalone",
+};
+
+module.exports = nextConfig;
+
